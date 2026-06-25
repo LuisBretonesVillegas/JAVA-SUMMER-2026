2ea29fd147ae2366117396bec6533a7261914a71
LuisBretonesVillegas <lbretonesvillegas@gmail.com>
2026-06-25 16:44:25 +0200

    tema_1 annotation fixed

--- src/Semana_1/tema_1/Ejercicio1_11.java
+++ src/Semana_1/tema_1/Ejercicio1_11.java
@@ -20,22 +20,6 @@ public class Ejercicio1_11 {
         System.out.println("The population in the year 3 is: " + year3);
         System.out.println("The population in the year 4 is: " + year4);
         System.out.println("The population in the year 5 is: " + year5);
-/* Esto se puede resolver con un bucle tambien (Solución de gémini):
-package Semana_1.tema_1;
 
-public class Ejercicio1_11 {
-    public static void main(String[] args) {
-
-        double secondsInYear = 365.0 * 24 * 60 * 60;
-        double netGrowthPerYear = (secondsInYear / 7.0) + (secondsInYear / 45.0) - (secondsInYear / 13.0);
-        double population = 312032486;
-
-        for (int year = 1; year <= 5; year++) {
-            population += netGrowthPerYear;
-            System.out.println("The population in the year " + year + " is: " + (int) population);
-        }
-    }
-}
- */
     }
 }
